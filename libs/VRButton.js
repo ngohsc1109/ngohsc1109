e0c116bbd77c9d3b1c38ae4bbf0568ce93dc2125
ngohsc1109 <134503767+ngohsc1109@users.noreply.github.com>
2023-07-10 16:07:35 +0800

    updated fotn and colour

--- libs/VRButton.js
+++ libs/VRButton.js
@@ -187,8 +187,8 @@ class VRButton{
         element.style.border = '1px solid #fff';
         element.style.borderRadius = '4px';
         element.style.background = (active) ? 'rgba(20,150,80,1)' : 'rgba(180,20,20,1)';
-        element.style.color = '#fff';
-        element.style.font = `normal ${fontSize}px sans-serif`;
+        element.style.color = '#00f';
+        element.style.font = `normal ${fontSize}px arial`;
         element.style.textAlign = 'center';
         element.style.opacity = '0.5';
         element.style.outline = 'none';
