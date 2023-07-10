622ba69434095f2c6dce0bec4ea588c43aadfd60
ngohsc1109 <134503767+ngohsc1109@users.noreply.github.com>
2023-07-10 16:10:13 +0800

    updated button

--- libs/VRButton.js
+++ libs/VRButton.js
@@ -185,7 +185,7 @@ class VRButton{
         element.style.bottom = '20px';
         if (!ignorePadding) element.style.padding = '12px 6px';
         element.style.border = '1px solid #fff';
-        element.style.borderRadius = '4px';
+        element.style.borderRadius = '5px';
         element.style.background = (active) ? 'rgba(20,150,80,1)' : 'rgba(180,20,20,1)';
         element.style.color = '#00f';
         element.style.font = `normal ${fontSize}px arial`;
